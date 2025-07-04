49d3c88d4799b16be6549968a023feaf663b4737
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-05 07:25:49 +0900

    feat: define get /users/:username/posts/:id route

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,14 @@ import { stringifyEntities } from "stringify-entities";
 import db from "./db.ts";
 import fedi from "./federation.ts";
 import type { Actor, Post, User } from "./schema.ts";
-import { FollowerList, Home, Layout, Profile, SetupForm } from "./views.tsx";
+import {
+  FollowerList,
+  Home,
+  Layout,
+  PostPage,
+  Profile,
+  SetupForm,
+} from "./views.tsx";
 
 const logger = getLogger("fedify-example");
 
@@ -85,6 +92,42 @@ app.get("/users/:username/followers", async (c) => {
   return c.html(
     <Layout>
       <FollowerList followers={followers} />
+    </Layout>,
+  );
+});
+app.get("/users/:username/posts/:id", (c) => {
+  const post = db
+    .prepare(
+      `
+      SELECT users.*, actors.*, posts.*
+      FROM posts
+      JOIN actors ON actors.id = posts.actor_id
+      JOIN users ON users.id = actors.user_id
+      WHERE users.username = ? AND posts.id = ?
+      `,
+    )
+    .get<Post & Actor & User>(c.req.param("username"), c.req.param("id"));
+  if (post == null) return c.notFound();
+
+  // biome-ignore lint/style/noNonNullAssertion: 언제나 하나의 레코드를 반환
+  const { followers } = db
+    .prepare(
+      `
+      SELECT count(*) AS followers
+      FROM follows
+      WHERE follows.following_id = ?
+      `,
+    )
+    .get<{ followers: number }>(post.actor_id)!;
+  return c.html(
+    <Layout>
+      <PostPage
+        name={post.name ?? post.username}
+        username={post.username}
+        handle={post.handle}
+        followers={followers}
+        post={post}
+      />
     </Layout>,
   );
 });
