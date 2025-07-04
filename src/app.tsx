70097a4853276cf3613c1e6453cd43f316b991fb
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-05 02:47:18 +0900

    feat: define post /setup route

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
 import { federation } from "@fedify/fedify/x/hono";
 import { Hono } from "@hono/hono";
 import { getLogger } from "@logtape/logtape";
+import db from "./db.ts";
 import fedi from "./federation.ts";
+import type { User } from "./schema.ts";
 import { Layout, SetupForm } from "./views.tsx";
 
 const logger = getLogger("fedify-example");
@@ -9,12 +11,30 @@ const logger = getLogger("fedify-example");
 const app = new Hono();
 
 app.use(federation(fedi, () => undefined));
-app.get("/setup", (c) =>
-  c.html(
+app.post("/setup", async (c) => {
+  // 계정이 이미 있는지 검사
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  const form = await c.req.formData();
+  const username = form.get("username");
+  if (typeof username !== "string" || !username.match(/^[a-z0-9_-]{1,50}$/)) {
+    return c.redirect("/setup");
+  }
+  db.prepare("INSERT INTO users (username) VALUES (?)").run(username);
+  return c.redirect("/");
+});
+app.get("/setup", (c) => {
+  // 계정이 이미 있는지 검사
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  return c.html(
     <Layout>
       <SetupForm />
     </Layout>,
-  ));
+  );
+});
 app.get("/", (c) => c.text("Hello, Fedify!"));
 
 export default app;
