dfb2175284f8589f6fe7fda6417c4b4afb0f79fc
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-05 02:36:37 +0900

    feat: define Layout component

---
+++ src/views.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "@hono/hono/jsx";
+
+export const Layout: FC = ({ children }) => (
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="color-scheme" content="light dark" />
+      <title>Microblog</title>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+      />
+    </head>
+    <body>
+      <main class="container">{children}</main>
+    </body>
+  </html>
+);
