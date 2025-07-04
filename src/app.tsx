e643317da6e4a56dc625b6c9b651e4d653c86a9f
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-05 02:37:36 +0900

    feat: define /setup route

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,20 @@
-import { Hono } from "@hono/hono";
 import { federation } from "@fedify/fedify/x/hono";
+import { Hono } from "@hono/hono";
 import { getLogger } from "@logtape/logtape";
 import fedi from "./federation.ts";
+import { Layout, SetupForm } from "./views.tsx";
 
 const logger = getLogger("fedify-example");
 
 const app = new Hono();
-app.use(federation(fedi, () => undefined))
 
+app.use(federation(fedi, () => undefined));
+app.get("/setup", (c) =>
+  c.html(
+    <Layout>
+      <SetupForm />
+    </Layout>,
+  ));
 app.get("/", (c) => c.text("Hello, Fedify!"));
 
 export default app;
