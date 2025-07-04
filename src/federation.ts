bfb4cc3c8f50d19d13f55f1057f751bee5e9a7c0
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-05 04:59:56 +0900

    feat: set key pairs dispatcher

--- src/federation.ts
+++ src/federation.ts
@@ -1,13 +1,16 @@
 import {
   createFederation,
   Endpoints,
+  exportJwk,
+  generateCryptoKeyPair,
+  importJwk,
   InProcessMessageQueue,
   MemoryKvStore,
   Person,
 } from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
 import db from "./db.ts";
-import type { Actor, User } from "./schema.ts";
+import type { Actor, Key, User } from "./schema.ts";
 
 const logger = getLogger("fedify-example");
 
@@ -29,6 +32,8 @@ federation.setActorDispatcher(
       )
       .get<User & Actor>(identifier);
     if (user == null) return null;
+
+    const keys = await ctx.getActorKeyPairs(identifier);
     return new Person({
       id: ctx.getActorUri(identifier),
       preferredUsername: identifier,
@@ -38,9 +43,58 @@ federation.setActorDispatcher(
         sharedInbox: ctx.getInboxUri(),
       }),
       url: ctx.getActorUri(identifier),
+      publicKey: keys[0].cryptographicKey,
+      assertionMethods: keys.map((k) => k.multikey),
     });
   },
-);
+).setKeyPairsDispatcher(async (ctx, identifier) => {
+  const user = db
+    .prepare("SELECT * FROM users WHERE username = ?")
+    .get<User>(identifier);
+  if (user == null) return [];
+  const rows = db
+    .prepare("SELECT * FROM keys WHERE keys.user_id = ?")
+    .all<Key>(user.id);
+  const keys = Object.fromEntries(
+    rows.map((row) => [row.type, row]),
+  ) as Record<Key["type"], Key>;
+  const pairs: CryptoKeyPair[] = [];
+  // 사용자가 지원하는 두 키 형식 (RSASSA-PKCS1-v1_5 및 Ed25519) 각각에 대해
+  // 키 쌍을 보유하고 있는지 확인하고, 없으면 생성 후 데이터베이스에 저장:
+  for (const keyType of ["RSASSA-PKCS1-v1_5", "Ed25519"] as const) {
+    if (keys[keyType] == null) {
+      logger.debug(
+        "The user {identifier} does not have an {keyType} key; creating one...",
+        { identifier, keyType },
+      );
+      const { privateKey, publicKey } = await generateCryptoKeyPair(keyType);
+      db.prepare(
+        `
+          INSERT INTO keys (user_id, type, private_key, public_key)
+          VALUES (?, ?, ?, ?)
+          `,
+      ).run(
+        user.id,
+        keyType,
+        JSON.stringify(await exportJwk(privateKey)),
+        JSON.stringify(await exportJwk(publicKey)),
+      );
+      pairs.push({ privateKey, publicKey });
+    } else {
+      pairs.push({
+        privateKey: await importJwk(
+          JSON.parse(keys[keyType].private_key),
+          "private",
+        ),
+        publicKey: await importJwk(
+          JSON.parse(keys[keyType].public_key),
+          "public",
+        ),
+      });
+    }
+  }
+  return pairs;
+});
 federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
 
 export default federation;
