51478c6fdbcbbc4c0f7c843bd1bc389c347b3b87
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-08 17:45:36 +0900

    refac: delete unused files

--- src/db.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from "sqlite";
-
-const db = new Database("microblog.sqlite3");
-db.exec("pragma journal_mode = WAL");
-db.exec("pragma foreign_keys = ON");
-
-export default db;
