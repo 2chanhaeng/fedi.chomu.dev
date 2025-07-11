80046bae1f76267147767be6d871022a10dc5ff2
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-07-11 16:49:33 +0900

    fix: fix middleware usage order

--- app.ts
+++ app.ts
@@ -11,8 +11,8 @@ const logger = getLogger("fedify-example");
 const app = new Hono({ strict: true });
 
 app.use(trimTrailingSlash());
+app.use(federation(fedi, () => undefined));
 app.use("*", authMiddleware); // 모든 라우트에 인증 미들웨어 적용
 app.route("/", router);
-app.use(federation(fedi, () => undefined));
 
 export default app;
